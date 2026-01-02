9116f7da43a947b6d6a21b73ae1ec63a19344809
sameer <sameeralhaddad38@gmail.com>
2026-01-02 08:21:25 +0100

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const env = {
+export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
 };
