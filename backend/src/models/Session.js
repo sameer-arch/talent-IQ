5bb13c1d6a6438db354c8e34c1cde84af9f050f3
sameer <sameeralhaddad38@gmail.com>
2026-02-11 10:26:54 +0100

    fixing session model

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -8,7 +8,7 @@ const sessionSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      enum: ["easy", "meduim", "hard"],
+      enum: ["easy", "medium", "hard"],
       required: true,
     },
     host: {
